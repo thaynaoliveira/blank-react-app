c00540efc89276c59cb94ba24becd26dd2ceaa56
Thayná Oliveira <thaynarnr@hotmail.com>
2019-07-10 11:12:21 +0200

    updating App component to give an example of calling an end point

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { funcGet } from "../actions";
 
-const App = () => (
-    <div>
-        React Blank Project
-    </div>
-);
-export default App;
+class App extends Component {
+    componentDidMount(){
+        let { dispatch } = this.props;
+        // dispatch(funcGet());
+    }
+    render(){
+        return (
+            <div>
+                React Blank Project
+            </div>
+        )
+    }
+}
+function mapStateToProps(state) {
+    return {
+        test: state.test
+    };
+}
+
+export default connect(mapStateToProps)(App);
